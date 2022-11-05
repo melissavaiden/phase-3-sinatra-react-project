33673f9c875c0e668543692060e372144f40f96b
Melissa Vaiden <melissavaiden@gmail.com>
2022-11-05 13:06:36 -0400

    Added Seed Data to User Table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 puts "🌱 Seeding spices..."
 
 User.create(username: "cookingperson1004")
+User.create(username: "SusanSmith")
+User.create(username: "JeffereyBurger")
+User.create(username: "lazycook123")
+User.create(username: "homechef100")
+User.create(username: "JohnDoe")
+User.create(username: "LeslieKnope")
+User.create(username: "TomHitch")
+User.create(username: "besthomecook")
+User.create(username: "MelissaV")
+User.create(username: "GabriellaN")
+User.create(username: "ChloeKing")
+User.create(username: "superchef1")
+
 
 puts "✅ Done seeding!"
